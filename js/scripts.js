01f4891cf3339bc7f9687feb638bdbf07d4ab987
Jahongir-veb <jahongirmh@gmail.com>
2021-01-07 21:50:07 +0500

    added new functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,29 @@
-var findMovies = function (searchRegex, minimumRating = 0, genre) {
-  return movies.filter(function (movie) {
-    var doesMatchCategory = genre === 'All' || movie.categories.includes(genre);
 
-    return movie.title.match(searchRegex) && movie.imdbRating >= minimumRating && doesMatchCategory;
-  });
+
+// DOM ga oid narsalar
+
+var elSearchForm = $_(`.js-search-form`);
+if (elSearchForm) {
+var elTitleInput = $_(`.js-search-form__title-input`, elSearchForm);
+var elRaitingInput = $_(`.js-search-form__rating-input`, elSearchForm);
+var elGenreInput = $_(`.js-search-form__genre-select`, elSearchForm);
+var elSortingSelect = $_(`.js-search-form__sort-select`, elSearchForm);
 };
 
-// DOM
-var elSearchForm = $_('.js-search-form');
-if (elSearchForm) {
-  var elTitleInput = $_('.js-search-form__title-input', elSearchForm);
-  var elRatingInput = $_('.js-search-form__rating-input', elSearchForm);
-  var elGenreSelect = $_('.js-search-form__genre-select', elSearchForm);
-  var elSortingSelect = $_('.js-search-form__sort-select', elSearchForm);
-}
 
-var elBookmarkedMovies = $_('.bookmarked-movies');
+var elBookmarkedMovies = $_(`.bookmarked-movies`);
 
-var elSearchResults = $_('.search-results');
+var elSearchResults = $_(`.search-results`);
 
-var elSearchResultTemplate = $_('#search-result-template').content;
-var elBookmarkedMovieTemplate = $_('#bookmarked-movie-template').content;
-var elPaginationItemTemplate = $_('#pagination-item-template').content;
+var elSearchResultTemplate = $_(`.#search-result-template`).content;
+var elBookmarkedMovieTemplate = $_(`.#bookmarked-movie-template`).content;
+var elPlaginationItemTemplate = $_(`.#pagination-item-template`).content;
 
-// DOM helper functions
+// DOM yordamchi funksiyalar
+
 var createMovieCard = function (movie) {
   var elMovie = elSearchResultTemplate.cloneNode(true);
 
-  // $_('.movie', elMovie) = ;
   $_('.movie__poster', elMovie).src = movie.smallPoster;
   $_('.movie__poster', elMovie).alt = `Poster of ${movie.title}`;
   $_('.movie__title', elMovie).textContent = movie.title;
@@ -39,26 +35,3 @@ var createMovieCard = function (movie) {
 
   return elMovie;
 };
-
-var displayMovies = function (movies) {
-  var elMoviesFragment = document.createDocumentFragment();
-  elSearchResults.innerHTML = '';
-
-  movies.forEach(function (movie) {
-    elMoviesFragment.appendChild(createMovieCard(movie));
-  });
-
-  elSearchResults.appendChild(elMoviesFragment);
-};
-
-
-// EVENT LISTENERS
-elSearchForm.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-
-  var searchRegex = new RegExp(elTitleInput.value.trim(), 'gi');
-  var minimumRating = Number(elRatingInput.value);
-  var genre = elGenreSelect.value;
-
-  displayMovies(findMovies(searchRegex, minimumRating, genre));
-});
